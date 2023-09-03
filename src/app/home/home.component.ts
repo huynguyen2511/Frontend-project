fd7103599b42dc64da14d1fe7f32116e71314035
huy25112001 <77772217+huy25112001@users.noreply.github.com>
2023-09-03 17:13:10 +0700

    vd15

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,15 @@ export class HomeComponent implements OnInit{
   public name = 'Huy';
   public age = 18
   public traiCay =['tao','nho','cam','quit'];
+  public traiCay2 =[{ten : 'tao', gia: 12, haGia: true},
+  {ten : 'nho', gia: 13, haGia: false},
+  {ten : 'cam', gia: -14, haGia: true},
+  {ten : 'quit', gia: 15, haGia: false}];
 
   constructor() {}
   ngOnInit(): void { 
-
+    console.log('trai cay = ', this.traiCay2);
+    
   }
 
   // public resetName(): void{
