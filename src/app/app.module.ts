5d872678b81ad6fbbaac779e02ccb9cbd05d3bb9
huy25112001 <77772217+huy25112001@users.noreply.github.com>
2023-09-04 17:02:56 +0700

    Login system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { OnSalePipe } from './on-sale.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    OnSalePipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
