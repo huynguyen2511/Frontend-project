3b6bffd20b10c868cd5baa23aea411fb98c0ae1a
huy25112001 <77772217+huy25112001@users.noreply.github.com>
2023-09-16 15:18:41 +0700

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
+  email: string ="";
+  password : string ="";
 
-}
+  isLogin: boolean = true;
+
+  errorMsg: string = "";
+
+  constructor (private router:Router ,private http : HttpClient) {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+    
+    let bodyData={
+      email : this.email,
+      password: this.password
+    };
+
+    this.http.post("http://localhost:5000/api/v1/auth/login", bodyData).subscribe((resultData: any) =>{
+      console.log(resultData);
+
+      if(resultData.status){
+
+        this.router.navigateByUrl('')
+      }
+      else{
+        alert("Incorrect email or password");
+        console.log("Error Login");
+        
+      }
+    })
+  }
+} 
